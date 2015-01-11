e29e4ce8ef65e22f4039322e3606808579a2437a
pnmichalakis <pnmichalakis@gmail.com>
2015-01-11 00:17:25 -0500

    truncates descriptions for projects, display with on hover function

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 	$("#swellexpanded").hide();
 	$("#fisticuffsexpanded").hide();
+	$("#fisticuffs").hover(function(){
+			$("#fisticuffsexpanded").show();
+	}, function(){
+		$("#fisticuffsexpanded").hide();
+	});
+	$("#swell").hover(function(){
+			$("#swellexpanded").show();
+	}, function(){
+		$("#swellexpanded").hide();
+	});
 });
